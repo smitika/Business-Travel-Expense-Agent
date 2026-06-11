a6276d542cf14d925488d7c671788527ba731fef
smitika <smitikapandit@gmail.com>
2026-06-12 02:17:41 +0530

    backend deployed

--- Frontend/src/api/client.js
+++ Frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000";
+const API = "https://business-travel-expense-agent.onrender.com";
 
 //receipt upload
 export const uploadFile = async (file) => {
